8ab045ca448d1adec50f8f141060b87b832eeecb
nguyenquan17 <50197780+nguyenquan17@users.noreply.github.com>
2019-05-25 20:32:13 +0700

    Add files via upload

--- CongTyABC/mainCT.cpp
+++ CongTyABC/mainCT.cpp
@@ -8,8 +8,14 @@
 using namespace std;
 int main()
 {
-    NhanVienHD a("123456","NguyenAnhQuan", "sadsda",10.000000,8.000000,41);
-    a.getSalary();
-    cout<<a.getSalary()<<endl;
+
+    NhanVienHD a("123456","NguyenAnhQuan", "sadsda",100,80,41);
+    NhanVienBC b("123456","NguyenAnhQuan","234", 10000);
+    NhanVienHH c("123456","NguyenAnhQuan","234", 10000, 200);
+    NhanVienHHDB d("123456","NguyenAnhQuan","234", 2000, 300, 1000);
+    cout<<"Luong nhan vien a: "<<a.getSalary()<<endl;
+    cout<<"Luong nhan vien b: "<<b.getSalary()<<endl;
+    cout<<"Luong nhan vien c: "<<c.getSalary()<<endl;
+    cout<<"Luong nhan vien d: "<<d.getSalary()<<endl;
     return 0;
 }
